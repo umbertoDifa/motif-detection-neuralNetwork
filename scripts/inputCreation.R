ced16eab9dbe2b6f73f40136c71b5484dda35686
Umberto Di Fabrizio <umberto.difabrizio@mail.polimi.it>
2015-11-01 19:42:42 -0700

    datacollection completed

---
+++ scripts/inputCreation.R
@@ -0,0 +1,6 @@
+#use data already enhanced
+PS00061 <- read.csv("C:/Users/Umberto/Desktop/projectNN/data/plus/PS00061.csv", stringsAsFactors=FALSE)
+PS50007 <- read.csv("C:/Users/Umberto/Desktop/projectNN/data/plus/PS50007.csv", stringsAsFactors=FALSE)
+PS50893 <- read.csv("C:/Users/Umberto/Desktop/projectNN/data/plus/PS50893.csv", stringsAsFactors=FALSE)
+PS51092 <- read.csv("C:/Users/Umberto/Desktop/projectNN/data/plus/PS51092.csv", stringsAsFactors=FALSE)
+PS51722 <- read.csv("C:/Users/Umberto/Desktop/projectNN/data/plus/PS51722.csv", stringsAsFactors=FALSE)
